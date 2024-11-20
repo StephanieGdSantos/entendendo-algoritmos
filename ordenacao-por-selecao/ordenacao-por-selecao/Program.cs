7781e185eb7e54335e6dd51c38c8c63402176a92
StephanieGdSantos <gomess214@gmail.com>
2024-11-20 13:50:53 -0300

    adiciona algoritmo

---
+++ ordenacao-por-selecao/ordenacao-por-selecao/Program.cs
@@ -0,0 +1,29 @@
+﻿using ordenacao_por_selecao.Services;
+
+var musicasMaisOuvidas = new Dictionary<string, int>()
+{
+    { "Blinding Lights - The Weeknd", 25 },
+    { "Someone Like You - Adele", 18 },
+    { "Shape of You - Ed Sheeran", 32 },
+    { "Levitating - Dua Lipa", 22 },
+    { "Bohemian Rhapsody - Queen", 12 },
+    { "Smells Like Teen Spirit - Nirvana", 15 },
+    { "Rolling in the Deep - Adele", 20 },
+    { "Perfect - Ed Sheeran", 28 },
+    { "Take On Me - A-ha", 14 },
+    { "Stay - The Kid LAROI & Justin Bieber", 30 },
+    { "Yesterday - The Beatles", 17 },
+    { "Bad Guy - Billie Eilish", 19 },
+    { "Hotel California - Eagles", 10 },
+    { "Lose Yourself - Eminem", 21 },
+    { "Don't Start Now - Dua Lipa", 16 }
+};
+
+var musicasEmOrdemDeOuvintes = Ordenador.OrdernarDeFormaDecrescente(musicasMaisOuvidas);
+
+Console.WriteLine("As músicas mais ouvidas em ordem de ouvintes são:");
+
+foreach (var musica in musicasEmOrdemDeOuvintes.Keys)
+{
+    Console.WriteLine($"{musica}: {musicasEmOrdemDeOuvintes[musica]}");
+}
---
+++ ordenacao-por-selecao/ordenacao-por-selecao/Services/Ordenador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordenacao_por_selecao.Services
+{
+    internal class Ordenador
+    {
+        public static Dictionary<string, int> OrdernarDeFormaCrescente(Dictionary<string, int> listaDesordenada)
+        {
+            string itemDeMaiorRelevancia;
+            int relevanciaDoItem;
+            var itensEmOrdemDeRelevancia = new Dictionary<string, int>();
+
+            while (listaDesordenada.Count != 0)
+            {
+                itemDeMaiorRelevancia = listaDesordenada.Keys.First();
+                relevanciaDoItem = listaDesordenada.Values.First();
+                foreach (var item in listaDesordenada.Keys)
+                {
+                    if (relevanciaDoItem < listaDesordenada[item])
+                    {
+                        itemDeMaiorRelevancia = item;
+                        relevanciaDoItem = listaDesordenada[item];
+                    }
+                }
+                itensEmOrdemDeRelevancia.Add(itemDeMaiorRelevancia, relevanciaDoItem);
+                listaDesordenada.Remove(itemDeMaiorRelevancia);
+            }
+
+            return itensEmOrdemDeRelevancia;
+        }
+
+        public static Dictionary<string, int> OrdernarDeFormaDecrescente(Dictionary<string, int> listaDesordenada)
+        {
+            string itemDeMaiorRelevancia;
+            int relevanciaDoItem;
+            var itensEmOrdemDeRelevancia = new Dictionary<string, int>();
+
+            while (listaDesordenada.Count != 0)
+            {
+                itemDeMaiorRelevancia = listaDesordenada.Keys.First();
+                relevanciaDoItem = listaDesordenada.Values.First();
+                foreach (var item in listaDesordenada.Keys)
+                {
+                    if (relevanciaDoItem < listaDesordenada[item])
+                    {
+                        itemDeMaiorRelevancia = item;
+                        relevanciaDoItem = listaDesordenada[item];
+                    }
+                }
+                itensEmOrdemDeRelevancia.Add(itemDeMaiorRelevancia, relevanciaDoItem);
+                listaDesordenada.Remove(itemDeMaiorRelevancia);
+            }
+
+            return itensEmOrdemDeRelevancia;
+        }
+    }
+}
