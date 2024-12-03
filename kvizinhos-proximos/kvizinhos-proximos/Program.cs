65e25d0ed2b68c9c21d92fbbfdc265b98641b8af
StephanieGdSantos <gomess214@gmail.com>
2024-12-02 22:56:24 -0300

    adiciona lista de usuarios

---
+++ kvizinhos-proximos/kvizinhos-proximos/Program.cs
@@ -0,0 +1,51 @@
+﻿var usuarios = new Dictionary<string, Dictionary<string, double>>
+{
+    { "Andreia", new Dictionary<string, double>
+        {
+            {"romance", 5 },
+            {"terror", 10 },
+            {"suspense", 5.5 },
+            {"comédia", 8.5 }
+        }
+    },
+    { "Luciana", new Dictionary<string, double>
+        {
+            {"romance", 8 },
+            {"terror", 7 },
+            {"suspense", 4 },
+            {"comédia", 5.5 }
+        }
+    },
+    { "Paulo", new Dictionary<string, double>
+        {
+            {"romance", 4 },
+            {"terror", 7 },
+            {"suspense", 5 },
+            {"comédia", 7 }
+        }
+    },
+    { "Cesar", new Dictionary<string, double>
+        {
+            {"romance", 2 },
+            {"terror", 1 },
+            {"suspense", 10 },
+            {"comédia", 5 }
+        }
+    },
+    { "Juliana", new Dictionary<string, double>
+        {
+            {"romance", 4 },
+            {"terror", 10 },
+            {"suspense", 5 },
+            {"comédia", 10 }
+        }
+    },
+    { "Otavio", new Dictionary<string, double>
+        {
+            {"romance", 9 },
+            {"terror", 0 },
+            {"suspense", 5 },
+            {"comédia", 7 }
+        }
+    }
+};
